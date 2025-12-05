3aa82a6dc9a0f3234a10ad858192d0699bfc762d
Gonzalo <gonzalo.patino@se.com>
2025-12-05 11:52:00 -0800

    Fix redirecting to home page after logging in

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import views as auth_views
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
+
+from django.contrib.auth.decorators import login_required
+
 
 
 urlpatterns = [
@@ -11,4 +15,12 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    
+# Named home route (protected)
+    path(
+        "",
+        login_required(TemplateView.as_view(template_name="home.html")),
+        name="home",
+    ),
+
 ]
