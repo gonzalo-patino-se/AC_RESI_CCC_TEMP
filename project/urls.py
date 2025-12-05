f56740c85b262cddeabf31c0da22ea3d7f1d42c3
Gonzalo <gonzalo.patino@se.com>
2025-12-04 16:10:54 -0800

    feature[auth]: Add minimum registration/login with built-in LoginView resolves

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.contrib.auth import views as auth_views
+
 from django.http import HttpResponse
 
 def home(request):
@@ -24,4 +27,10 @@ def home(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home), #root path
+    path("accounts/", include("django.contrib.auth.urls")), 
+    
+    path("accounts/login/", auth_views.LoginView.as_view(
+        template_name="registration/login.html"
+    ), name="login"),
+
 ]
